819c27d877ddec29485da6bb06a783ba44327503
Anup kumar chakra <88049424+Codinghub30@users.noreply.github.com>
2024-03-07 00:33:23 +0530

    Profile - deleteAccount and signout and sign out of sidebar and header

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { TextInput, Button, Alert } from "flowbite-react";
+import { TextInput, Button, Alert, Modal, ModalHeader } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import {
@@ -11,14 +11,20 @@ import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import {
-  updateSuccess,
-  updateFailure,
-  updateStart,
+    updateStart,
+    updateSuccess,
+    updateFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
+    signoutSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading , error } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgess, setImageFileUploadProgess] = useState(null);
@@ -26,6 +32,7 @@ export default function DashProfile() {
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
   const [updateUserError, setUpdateUserError] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [formData, setFormData] = useState({});
   const filePickerRef = useRef();
   const dispatch = useDispatch();
@@ -80,6 +87,8 @@ export default function DashProfile() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
     if (Object.keys(formData).length === 0) {
       setUpdateUserError("No changes made");
       return;
@@ -111,6 +120,41 @@ export default function DashProfile() {
       setUpdateUserError(error.message);
     }
   };
+
+  const handleDeleteUser = async () => {
+    setShowModal(false);
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(deleteUserFailure(data.message));
+      } else {
+        dispatch(deleteUserSuccess(data));
+      }
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto my-0 p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl ">Profiling</h1>
@@ -182,9 +226,10 @@ export default function DashProfile() {
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
+        </form>
         <div className="text-red-500 flex justify-between mt-5">
-          <span className="cursor-pointer">Delete Account</span>
-          <span className="cursor-pointer"> Sign Out</span>
+          <span onClick={() => setShowModal(true)} className="cursor-pointer">Delete Account</span>
+          <span  onClick={handleSignout} className="cursor-pointer"> Sign Out</span>
         </div>
         {updateUserSuccess && (
           <Alert color="success" className="mt-5">
@@ -197,7 +242,25 @@ export default function DashProfile() {
             {updateUserError}
           </Alert>
         )}
-      </form>
+          {error && (
+          <Alert color="failure" className="mt-5">
+            {error}
+          </Alert>
+        )}
+        <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+            <ModalHeader/>
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                    <h3>Are you sure you want to delete your account?</h3>
+                    <div className="flex justify-center gap-4">
+                        <Button color="failure" onClick={handleDeleteUser}> Yes I'm Sure</Button>
+                        <Button color="gray" onClick={() => setShowModal(false)}>No,cancel </Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+      
     </div>
   );
 }
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,11 +1,29 @@
 import { Sidebar, SidebarItem } from "flowbite-react";
 import { HiUser, HiArrowSmRight } from 'react-icons/hi'
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function DashSidebar(){
     const location = useLocation();
+    const dispatch = useDispatch();
     const [tab , setTab ]  = useState('');
+    const handleSignout = async () => {
+        try {
+          const res = await fetch('/api/user/signout', {
+            method: 'POST',
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            console.log(data.message);
+          } else {
+            dispatch(signoutSuccess());
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
         const  tabFromUrl = urlParams.get('tab');
@@ -15,13 +33,14 @@ export default function DashSidebar(){
         }
       }, [location.search]);
     return ( 
-        <Sidebar>
+        <Sidebar  className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
+                    <Link to='/dashboard?tab=profile'> </Link>
                     <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>
                     Profile
                     </Sidebar.Item>
-                    <Sidebar.Item active icon={HiArrowSmRight}  className='cursor-pointer'>
+                    <Sidebar.Item active icon={HiArrowSmRight} on className='cursor-pointer' onClick={handleSignout} >
                     Sign Out
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
